dc21f8b14431961bcc6edc8a09fa438c0b895d81
sz <sz@3135814698.com>
2021-01-16 12:18:44 +0800

    commit

--- src/com/sz/domain/User.java
+++ src/com/sz/domain/User.java
@@ -5,4 +5,36 @@ public class User {
     private String name;
     private int age;
     private String classroomId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(String classroomId) {
+        this.classroomId = classroomId;
+    }
 }
